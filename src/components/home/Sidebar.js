76a6e4fb8704e4b2a66bc3a87d02e5ad567706ba
numangit <abdulnumanjob@gmail.com>
2023-03-22 17:05:32 +0600

    team memember component created

--- src/components/home/Sidebar.js
+++ src/components/home/Sidebar.js
@@ -1,52 +1,15 @@
 import React from 'react';
 import ProjectList from './ProjectList';
+import TeamMembers from './TeamMembers';
 
 const Sidebar = () => {
     return (
         <div class="sidebar">
-            {/* <!-- Projects List --> */}
+
             <ProjectList />
 
-            {/* <!-- Team Members --> */}
-            <div class="mt-8">
-                <h3 class="text-xl font-bold">Team Members</h3>
-                <div class="mt-3 space-y-4">
-                    <div class="checkbox-container">
-                        <img src="./images/avatars/sumit.png" class="team-avater" />
-                        <p class="label">Sumit Saha</p>
-                    </div>
+            <TeamMembers />
 
-                    <div class="checkbox-container">
-                        <img src="./images/avatars/sadh.png" class="team-avater" />
-                        <p class="label">Sadh Hasan</p>
-                    </div>
-
-                    <div class="checkbox-container">
-                        <img src="./images/avatars/akash.png" class="team-avater" />
-                        <p class="label">Akash Ahmed</p>
-                    </div>
-
-                    <div class="checkbox-container">
-                        <img src="./images/avatars/salahuddin.png" class="team-avater" />
-                        <p class="label">Md Salahuddin</p>
-                    </div>
-
-                    <div class="checkbox-container">
-                        <img src="./images/avatars/riyadh.png" class="team-avater" />
-                        <p class="label">Riyadh Hassan</p>
-                    </div>
-
-                    <div class="checkbox-container">
-                        <img src="./images/avatars/ferdous.png" class="team-avater" />
-                        <p class="label">Ferdous Hassan</p>
-                    </div>
-
-                    <div class="checkbox-container">
-                        <img src="./images/avatars/almas.png" class="team-avater" />
-                        <p class="label">Arif Almas</p>
-                    </div>
-                </div>
-            </div>
         </div>
     );
 };
