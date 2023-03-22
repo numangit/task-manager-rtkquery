9c25be0e97f23e11bec2ea2d0d41ec9bb232fdca
numangit <abdulnumanjob@gmail.com>
2023-03-22 19:18:59 +0600

    teamMemberApi created

---
+++ src/features/teamMember/teamMemberApi.js
@@ -0,0 +1,9 @@
+import { apiSlice } from "../api/apiSlice";
+
+export const teamMemberApi = apiSlice.injectEndpoints({
+    endpoints: (builder) => ({
+        getTeamMember: builder.query({
+            query: () => ({ url: "/team" })
+        })
+    })
+});
