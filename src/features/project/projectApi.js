3124a65a2c717e455580f132b80521cf77a28dbd
numangit <abdulnumanjob@gmail.com>
2023-03-22 18:28:18 +0600

    projects api added

---
+++ src/features/project/projectApi.js
@@ -0,0 +1,11 @@
+import { apiSlice } from "../api/apiSlice";
+
+export const projectApi = apiSlice.injectEndpoints({
+    endpoints: (builder) => ({
+        getProjects: builder.query({
+            query: () => ({ url: '/projects' }),
+        })
+    })
+});
+
+export const { useGetProjectsQuery } = projectApi;
