e80428f68adea0a36827497540dbefb020e00c85
numangit <abdulnumanjob@gmail.com>
2023-03-22 23:27:01 +0600

    task api mutation created

--- src/features/task/taskApi.js
+++ src/features/task/taskApi.js
@@ -4,8 +4,26 @@ const taskApi = apiSlice.injectEndpoints({
     endpoints: (builder) => ({
         getTasks: builder.query({
             query: () => ({ url: "/tasks" })
+        }),
+        getTask: builder.query({
+            query: (id) => ({ url: `/tasks/${id}` })
+        }),
+        addTask: builder.mutation({
+            query: (data) => ({ url: "/tasks", method: 'POST', body: data })
+        }),
+        editTask: builder.mutation({
+            query: ({ id, data }) => ({ url: `/tasks/${id}`, method: 'PATCH', body: data })
+        }),
+        deleteTask: builder.mutation({
+            query: (id) => ({ url: `/tasks/${id}`, method: 'PATCH' })
         })
     })
 });
 
-export const { useGetTasksQuery } = taskApi;
+export const {
+    useGetTasksQuery,
+    useGetTaskQuery,
+    useAddTaskMutation,
+    useEditTaskMutation,
+    useDeleteTaskMutation
+} = taskApi;
